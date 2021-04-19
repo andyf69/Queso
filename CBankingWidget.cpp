03c5f33ba9ad1279cc420db7e842bd3f4b0dc141
Andy French <andyf69@gmail.com>
2021-04-18 22:26:54 -0400

    Cosmetic enhancements to the banking account register

--- CBankingWidget.cpp
+++ CBankingWidget.cpp
@@ -2,6 +2,7 @@
 #include "CAccount.h"
 #include "CBankingRegisterDelegate.h"
 #include "CBankingRegisterModel.h"
+#include "CFinancialInstitution.h"
 #include <QtCore/QDebug>
 #include <QtSql/QSqlQuery>
 #include <QtSql/QSqlRelationalDelegate>
@@ -14,13 +15,17 @@ CBankingWidget::CBankingWidget(QWidget* pParent)
     ui.m_pRegisterTableView->setAlternatingRowColors(true);
     ui.m_pRegisterTableView->verticalHeader()->setVisible(false);
     ui.m_pRegisterTableView->setItemDelegate(new CBankingRegisterDelegate(ui.m_pRegisterTableView));
+    ui.m_pRegisterTableView->setSelectionBehavior(QTableView::SelectRows);
+    ui.m_pRegisterTableView->setSelectionMode(QTableView::SingleSelection);
 }
 
 void CBankingWidget::setAccount(const int iAccountId)
 {
     CAccount oAccount;
     oAccount.dbRead(iAccountId);
-    ui.m_pAccountName->setText(oAccount.name());
+    CFinancialInstitution oFI;
+    oFI.dbRead(oAccount.financialInstitutionId());
+    ui.m_pAccountName->setText(QString("%1 : %2").arg(oAccount.name()).arg(oFI.name()));
 
     CBankingRegisterModel* pModel = new CBankingRegisterModel(this);
     pModel->setAccount(iAccountId);
@@ -29,4 +34,5 @@ void CBankingWidget::setAccount(const int iAccountId)
     ui.m_pRegisterTableView->hideColumn(static_cast<int>(CBankingRegisterModel::Column::Id));
     ui.m_pRegisterTableView->hideColumn(static_cast<int>(CBankingRegisterModel::Column::SequenceNum));
     ui.m_pRegisterTableView->scrollToBottom();
+    ui.m_pRegisterTableView->resizeColumnsToContents();
 }
